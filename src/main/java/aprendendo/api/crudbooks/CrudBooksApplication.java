bfe5cb8248083e189c7a82faa09d6157249baf43
joaopaulo <joaopaulosg17@gmail.com>
2023-07-17 18:08:29 -0300

    commit inicial

---
+++ src/main/java/aprendendo/api/crudbooks/CrudBooksApplication.java
@@ -0,0 +1,13 @@
+package aprendendo.api.crudbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aprendendo/api/crudbooks/CrudBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package aprendendo.api.crudbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
